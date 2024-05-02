843347b053f1bb0999eb04ea0852a95b27ba73d3
Ivan Kovalenko <g0g0lm0g0l@protonmail.com>
2024-05-02 13:51:43 +0200

    basic file structure (no modular) and main.gd script attached to the main scene

---
+++ scripts/main.gd
@@ -0,0 +1,11 @@
+extends Node
+#The main.gd script would then serve as the primary controller of the game.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
