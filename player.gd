82427fb4b496b01785a2c428d848cdc1dbbfd5d0
Ivan Kovalenko <g0g0lm0g0l@protonmail.com>
2024-05-02 18:42:54 +0200

    camera was added in the previous commit. Improved mechanics, can't properly jump and attack, collisions are added, hierarqui seems pretty

--- player.gd
+++ player.gd
@@ -5,18 +5,27 @@ var current_direction = "none"
 var click_position = Vector2()
 var target_position = Vector2()
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 func _ready():
 	set_process_input(true)
 
+
+func _process(delta):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+
+
 func _physics_process(delta):
-	player_movement(delta)
+	if not is_on_floor():
+		self.velocity.y -= gravity * delta
+	_player_movement(delta)
 	
 
-	
-func player_movement(delta):
+func _player_movement(delta):
 	if Input.is_action_pressed("up"):
 		current_direction = "up"
-		play_animation(1)
+		_play_animation(1)
 		velocity.y = -SPEED
 		velocity.x = 0
 	elif Input.is_action_pressed("down"):
@@ -25,57 +34,48 @@ func player_movement(delta):
 		velocity.x = 0
 	elif Input.is_action_pressed("left"):
 		current_direction = "left"
-		play_animation(1)
+		_play_animation(1)
 		velocity.y = 0
 		velocity.x = -SPEED
 	elif Input.is_action_pressed("right"):
 		current_direction = "right"
-		play_animation(1)
+		_play_animation(1)
 		velocity.y = 0
 		velocity.x = SPEED
 	elif Input.is_action_pressed("jump"):
 		current_direction = "jump"
-		play_animation(1)
+		_play_animation(1)
 		velocity.y = 0
 		velocity.x = 0
 	elif Input.is_action_just_pressed("left_mouse_click_attack"):
+		current_direction = "attack"
+		_play_animation(1)
 		click_position = get_global_mouse_position()
-		play_animation(1)
 	else:
-		play_animation(0)
+		_play_animation(0)
 		velocity.y = 0
 		velocity.x = 0   
 	move_and_slide()
 	
 	
-func play_animation(movement):
+func _play_animation(movement):
 	var direction = current_direction	
 	var animation_player = $AnimatedSprite2D
-	if direction == "right":
-		animation_player.flip_h = false
-		if movement == 1:
+	if movement == 0:
+		animation_player.play("idle_main_character_front")
+	elif movement == 1:
+		if direction == "right":
+			animation_player.flip_h = false
 			animation_player.play("main_character_side_walk")
-		elif movement == 0:
-			animation_player.play("idle_main_character_front")
-	if direction == "left":
-		animation_player.flip_h = true
-		if movement == 1:
+		if direction == "left":
+			animation_player.flip_h = true
 			animation_player.play("main_character_side_walk")
-		elif movement == 0:
-			animation_player.play("idle_main_character_front")
-	if direction == "up":
-		if movement == 1:
+		if direction == "up":
 			animation_player.play("main_character_up_and_down")
-		elif movement == 0:
-			animation_player.play("idle_main_character_front")
-	if direction == "down":
-		if movement == 1:
+		if direction == "down":
 			animation_player.play("main_character_up_and_down")
-		elif movement == 0:
-			animation_player.play("idle_main_character_front")
-	if direction == "jump":
-		if movement == 1:
+		if direction == "jump":
 			animation_player.play("main_character_jump")
-		elif movement == 0:
-			animation_player.play("idle_main_character_front")
-		
+		if direction == "attack":
+			animation_player.play("main_character_attack")
+			
