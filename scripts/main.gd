cc479134186de7f8c4e1220c944aca6ccadf225d
Ivan Kovalenko <g0g0lm0g0l@protonmail.com>
2024-05-11 14:27:13 +0200

    Added simple platform scene with one way collition in the player scene changed CanvasItem -> Ordering -> Z index for pass behind the platform

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,9 +1,8 @@
 extends Node
-#The main.gd script would then serve as the primary controller of the game.
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	var world_scene = load("res://scenes/world.tscn")
+	var _world_scene = load("res://scenes/world.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
