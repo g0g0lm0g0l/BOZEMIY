bd4bb0d8b7224a1bb694c519999236f4eeca3f6c
Ivan Kovalenko <g0g0lm0g0l@protonmail.com>
2024-05-11 17:03:27 +0200

    Added enemy, with simple movements and default gravity, NO IA for the moment, refactored the script of the player, eliminated redundancy better gravity, still pure

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
 @export var speed = 100
-@export var gravity = 30
-@export var jump_force = 300
-const GRAVITY = 1000
+@export var jump_force = 300.0
+
+var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var current_direction = "none"
 var animation_player = null
@@ -12,26 +12,22 @@ var animation_player = null
 func _ready():
 	animation_player = $AnimatedSprite2D
 	animation_player.play("idle_main_character_front")
-
-
+	
+	
 func _process(delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
-
-
+		
+		
 func _physics_process(delta):
-	if !is_on_floor():
-		velocity.y += gravity
-		if velocity.y > 1000:
-			velocity.y = 1000
+	self.velocity.y += GRAVITY * delta
 	_player_movement(delta)
+	move_and_slide()
 	
-
+	
 func _player_movement(delta):
 	var horizontal_directions = Input.get_axis("left", "right")
 	velocity.x = speed * horizontal_directions
-	#move_and_collide(velocity * delta)
-	move_and_slide()
 	if Input.is_action_pressed("down"):
 		current_direction = "down"
 		_play_animation(1)
@@ -41,48 +37,34 @@ func _player_movement(delta):
 	elif Input.is_action_pressed("right"):
 		current_direction = "right"
 		_play_animation(1)
-	elif Input.is_action_pressed("jump") or  Input.is_action_pressed("up"):
+	elif Input.is_action_pressed("jump"):
 		velocity.y = -jump_force
 		current_direction = "jump"
 		_play_animation(1)
 	elif Input.is_action_just_pressed("left_mouse_click_attack"):
 		current_direction = "attack"
+		animation_player.play("main_character_attack")
 		_play_animation(1)
-	else:
+	else: 
 		_play_animation(0)
+
 	
 	
 func _play_animation(movement):
 	var direction = current_direction	
-	
-	if direction == "right":
-		if movement == 1:
-			animation_player.flip_h = false
-			animation_player.play("main_character_side_walk")
-		else:
-			animation_player.play("idle_main_character_front")
-	if direction == "left":
-		if movement == 1:
-			animation_player.flip_h = true
-			animation_player.play("main_character_side_walk")
-		else:
-			animation_player.play("idle_main_character_front")
-	if direction == "up":
-		if movement == 1:
-			animation_player.play("main_character_up_and_down")
-		else:
-			animation_player.play("idle_main_character_front")
-	if direction == "down":
-		if movement == 1:
-			animation_player.play("main_character_up_and_down")
-		else:
-			animation_player.play("idle_main_character_front")
-	if direction == "jump":
-		if movement == 1:
-			animation_player.play("main_character_jump")
-		else:
-			animation_player.play("idle_main_character_front")
 	if direction == "attack":
-		#TODO: change this
 		animation_player.play("main_character_attack")
-		movement = 0
+	else:
+		if movement > 0:
+			if direction == "right":
+				animation_player.flip_h = false
+				animation_player.play("main_character_side_walk")
+			elif direction == "left":
+				animation_player.flip_h = true
+				animation_player.play("main_character_side_walk")
+			elif direction == "down":
+				animation_player.play("main_character_up_and_down")
+			elif direction == "jump":
+				animation_player.play("main_character_jump")
+		else:
+			animation_player.play("idle_main_character_front")
