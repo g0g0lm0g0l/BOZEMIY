311b7e9cdfff5076a1bd2de0e8118355f0327313
Ivan Kovalenko <g0g0lm0g0l@protonmail.com>
2024-05-26 00:33:11 +0200

    Added 2 layer backgraund and improved some features

--- scripts/world.gd
+++ scripts/world.gd
@@ -6,6 +6,7 @@ const ENEMY = preload("res://scenes/enemy.tscn")
 
 const PLATFORM_COUNT = 120
 
+const INCREASE_Y_DISTANCE_PLATFORMS = 12
 # Watch the logic below
 const TOP_LIMIT_Y = -1000
 const BOTTOM_LIMIT_Y = 500
@@ -17,7 +18,7 @@ var random_number_generator = RandomNumberGenerator.new()
 func _ready():
 	for i in range(0, PLATFORM_COUNT):
 		var random_x: float = random_number_generator.randf_range(LEFT_LIMIT_X + i * 2, RIGHT_LIMIT_X)
-		var random_y: float = random_number_generator.randf_range(BOTTOM_LIMIT_Y - i, TOP_LIMIT_Y)
+		var random_y: float = BOTTOM_LIMIT_Y - i * INCREASE_Y_DISTANCE_PLATFORMS
 		add_platform(random_x, random_y)
 		if i % 2 == 0:
 			add_friut(random_x, random_y)
